f9c3932bb9756e483e04bd77c15fdfe6bf7af70b
Arya Boudaie <aboudaie@brandeis.edu>
2019-02-03 16:21:51 -0800

    Making the admin site all tricked up.

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,45 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register([Poll, TextChoicesQuestion, TextChoice,
-                    ChoiceVote, RankingQuestion, RankVote, Question, Vote])
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+
+
+class RankingQuestionInline(admin.TabularInline):
+    model = RankingQuestion
+
+
+class TextChoiceQuestionInline(admin.TabularInline):
+    model = TextChoicesQuestion
+
+
+class TextChoiceInline(admin.TabularInline):
+    model = TextChoice
+
+
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    inlines = [
+        QuestionInline,
+        TextChoiceQuestionInline,
+        RankingQuestionInline,
+    ]
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [
+        TextChoiceQuestionInline,
+        RankingQuestionInline,
+    ]
+
+
+@admin.register(TextChoicesQuestion)
+class QTextChoicesQuestionAdmin(admin.ModelAdmin):
+    inlines = [
+        TextChoiceInline
+    ]
+
+
+admin.site.register([TextChoice, ChoiceVote, RankingQuestion, RankVote, Vote])
