ca17800a447de9fab30eccba66c0cc1c89a64415
Arya Boudaie <aboudaie@brandeis.edu>
2019-02-08 01:11:36 -0800

    Add logged in only page.

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('yourpolls', views.your_polls, name='your_polls'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,21 +1,31 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Poll
 
+QUESTION_FIELDS = [
+    'question_set',
+    'question_set__rankingquestion',
+    'question_set__rankingquestion__rankvote_set',
+    'question_set__textchoicesquestion',
+    'question_set__textchoicesquestion__textchoice_set',
+    'question_set__textchoicesquestion__textchoice_set__choicevote_set',
+    'question_set__textchoicesquestion__textchoice_set__textchoicenuance_set',
+    'question_set__textchoicesquestion__textchoice_set__textchoicenuance_set__choicenuancevote_set',
+]
 
-# Create your views here.
 
 def index(request):
-    fields = [
-        'question_set', 'question_set__rankingquestion',
-        'question_set__rankingquestion__rankvote_set',
-        'question_set__textchoicesquestion',
-        'question_set__textchoicesquestion__textchoice_set',
-        'question_set__textchoicesquestion__textchoice_set__choicevote_set',
-        'question_set__textchoicesquestion__textchoice_set__textchoicenuance_set',
-        'question_set__textchoicesquestion__textchoice_set__textchoicenuance_set__choicenuancevote_set',
-
-    ]
-    return render(request, 'your_polls.html',
+    return render(request, 'list_polls.html',
                   context={
-                      'polls': Poll.objects.prefetch_related(*fields).all()
+                      'polls': Poll.objects.prefetch_related(
+                          *QUESTION_FIELDS).all()
+                  })
+
+
+@login_required
+def your_polls(request):
+    return render(request, 'list_polls.html',
+                  context={
+                      'polls': Poll.objects.prefetch_related(
+                          *QUESTION_FIELDS).filter(owner=request.user)
                   })
