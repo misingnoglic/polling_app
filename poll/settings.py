10b64352933ee2c5bf8d3c39862a3e1ffcce72ca
Arya Boudaie <aboudaie@brandeis.edu>
2019-03-11 22:38:08 -0700

    Implement simple logout.

--- poll/settings.py
+++ poll/settings.py
@@ -139,3 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/polls/'
+
+LOGOUT_REDIRECT_URL = '/polls/'
--- polls/urls.py
+++ polls/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('vote/<int:poll_id>', views.vote_on_poll, name='vote'),
     path('yourpolls', views.your_polls, name='your_polls'),
-    path('login', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login',
+         auth_views.LoginView.as_view(template_name='login.html'),
+         name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout')
 ]
